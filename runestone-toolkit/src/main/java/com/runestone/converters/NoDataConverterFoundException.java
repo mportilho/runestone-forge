7c45560a7f2d146bd99ea46c7e9f9e9b4617a490
Marcelo Portilho <marcelo.portilho@basa.com.br>
2023-11-29 23:03:50 -0300

    Better error message

--- runestone-toolkit/src/main/java/com/runestone/converters/NoDataConverterFoundException.java
+++ runestone-toolkit/src/main/java/com/runestone/converters/NoDataConverterFoundException.java
@@ -8,7 +8,7 @@ public class NoDataConverterFoundException extends RuntimeException {
     private final Class<?> targetType;
 
     public NoDataConverterFoundException(Class<?> sourceType, Class<?> targetType) {
-        super(String.format("No converter found for source [%s] and target [%s]",
+        super(String.format("No converter found for source %s and target %s",
                 sourceType.getCanonicalName(), targetType.getCanonicalName()));
         this.sourceType = Objects.requireNonNull(sourceType);
         this.targetType = Objects.requireNonNull(targetType);
